927a0b1f84e70cad4fab2675ad7cf8f1e42fb128
Caleb M Carroll <144181450+TheFa1seKing@users.noreply.github.com>
2023-09-22 13:51:11 -0700

    Update strings.js

--- strings.js
+++ strings.js
@@ -6,12 +6,13 @@ function CheckInput()
     var Name = firstName +  " " + LastName; // combines fname and lname to one string
     var len = Name.length; //checks the strings length
     var number = document.getElementById("number").value;// creates a string based on user input
-    var len2 = number.length;//checks the strings length
+    var reg = number.replace(/[^0-9]/g, "");// replace any non number to an empty string (ie, remove it)
+    var len2 = reg.length;//checks the strings length
     if (len >20) //if name is over 20 letters it is invalid
     {
         document.getElementById("loginStatus").innerHTML = "Invalid name length";
     }
-    else if (len2 == 10) //if number is 10 characters it is valid
+    else if (len2 == 10 ) //if number is 10 characters it is valid
     {
         document.getElementById("loginStatus").innerHTML =" Valid login!!!";
     }
